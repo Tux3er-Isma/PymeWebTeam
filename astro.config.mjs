a6abfff84217c4fab98897094396fbcea259473f
Tux3er <isma.tux3er@gmail.com>
2024-08-09 23:13:12 +0200

    ⬆️Adding netlify for backend

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify";
-
 import react from "@astrojs/react";
+
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: netlify()
 });
